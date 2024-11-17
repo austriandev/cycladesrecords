bf46446010820b44c1f94c475fe9d1c88d048e10
austriandev <avoliotisapplink@gmail.com>
2024-11-17 03:29:08 +0200

    Add GitHub Actions workflow

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/cycladesrecords',
   images: {
     unoptimized: true,
-  },
+  }
 };
 
 export default nextConfig;
